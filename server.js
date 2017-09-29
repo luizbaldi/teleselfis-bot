7914a6b0dc6fe8d920d78a9670702461040c5b9d
Luiz Baldi <baldilp@gmail.com>
2017-09-29 00:01:07 -0300

    small fix to heroku deploy

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 /* Config */
 const botConfig = require('./src/config/botConfig')();
+const http = require('http');
+const port = process.env.PORT || 5000;
 
 /* Controllers */
 const BotController = require('./src/controller/botController')();
@@ -9,3 +11,6 @@ const bot = botConfig.factoryBot();
 
 /* Start bot event listeners */
 BotController.startBotListeners(bot);
+http.createServer((request, response) => {
+    console.log(`Starting server at port: ${port}`);
+}).listen(port);
