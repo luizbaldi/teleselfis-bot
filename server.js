5b924ed99f194714b911b0f0f459ba3d7c1957ae
Luiz Baldi <baldilp@gmail.com>
2017-09-29 00:13:50 -0300

    heroku deploy changes two

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 /* Config */
 const botConfig = require('./src/config/botConfig')();
-const http = require('http');
-const port = process.env.PORT || 5000;
+const express = require('express');
+const bodyParser = require('body-parser');
 
 /* Controllers */
 const BotController = require('./src/controller/botController')();
@@ -10,7 +10,20 @@ const BotController = require('./src/controller/botController')();
 const bot = botConfig.factoryBot();
 
 /* Start bot event listeners */
-BotController.startBotListeners(bot);
-http.createServer((request, response) => {
-    console.log(`Starting server at port: ${port}`);
-}).listen(port);
+const app = express();
+app.use(bodyParser.json());
+
+app.get('/', function (req, res) {
+    res.json({ version: '1.0' });
+});
+
+const server = app.listen(process.env.PORT, "0.0.0.0", () => {
+    const host = server.address().address;
+    const port = server.address().port;
+    console.log('Web server started at http://%s:%s', host, port);
+});
+
+app.post('/' + bot.token, function (req, res) {
+    BotController.startBotListeners(bot);
+    res.sendStatus(200);
+});
