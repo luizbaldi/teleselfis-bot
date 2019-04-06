9f271e6b71dd87e74e8beef47616cd170a7aef23
arturbani <arturbani@gmail.com>
2019-04-06 16:34:35 -0300

    Token extracted from src

--- src/config/botConfig.js
+++ src/config/botConfig.js
@@ -1,15 +1,8 @@
 /* External modules */
 const TelegramBot = require('node-telegram-bot-api')
+const { token:potatoe } = require('./botTokens.js')
 
-/* @TODO: Move this token into a separe file (on .gitignore pls) */
-
-/* Telegram Bot Token */
-// const token = '338766426:AAFpjZIzU85KQhsujlDjXCC3DovPQIiVocE';
-
-/* Test bot token */
-const token = '473533364:AAGwn6f0jwqb58s9XBQuwcgLL6_a-FjcHeU'
-
-const factoryBot = () => new TelegramBot(token, {
+const factoryBot = () => new TelegramBot(potatoe, {
   polling: true
 })
 
